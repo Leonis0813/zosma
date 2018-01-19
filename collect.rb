731bf3cd87746f5e5fbffe28433ee45f0d0c6435
Leonis0813 <Leonis.0813@gmail.com>
2018-01-19 20:58:54 +0000

    create collect.rb

---
+++ collect.rb
@@ -0,0 +1,14 @@
+require 'date'
+require_relative 'collect/import'
+require_relative 'collect/export'
+require_relative 'config/settings'
+require_relative 'lib/logger'
+
+TARGET_DATE = Date.today - 2
+RATE_FILES = Dir[File.join(Settings.csv_dir, "*_#{TARGET_DATE.strftime('%F')}.csv")]
+
+RATE_FILES.each do |rate_file|
+  import_rates(rate_file)
+end
+
+export_rates(TARGET_DATE) unless RATE_FILES.empty?
