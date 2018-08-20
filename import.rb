c5030fba38bc2518c5710b79ac06addbb84cc5a9
Leonis0813 <Leonis.0813@gmail.com>
2018-08-20 06:03:13 +0100

    create import script

---
+++ import.rb
@@ -0,0 +1,52 @@
+require 'csv'
+require 'fileutils'
+require 'logger'
+require_relative 'config/initialize'
+require_relative 'db/connect'
+Dir['models/*'].each {|f| require_relative f }
+
+TARGET_DATE = (Date.today - 2).strftime('%F')
+TARGET_FILES = Dir[File.join(Settings.import.src_dir, "*_#{TARGET_DATE}.csv")]
+BACKUP_DIR = File.join(APPLICATION_ROOT, Settings.import.backup_dir)
+
+rates_file = 0
+
+Dir.mktmpdir(nil, File.join(APPLICATION_ROOT, Settings.import.tmp_dir)) do |dir|
+  tmp_file_name = File.join(dir, 'rates.csv')
+
+  TARGET_FILES.each do |file|
+    CSV.open(tmp_file_name, 'w') do |csv|
+      CSV.read(file, :converters => :all).each do |rate|
+        csv << [rate[0].strftime('%F %T'), rate[1], rate[2], rate[3]]
+      end
+      rates_file += csv.size
+    end
+
+    id = Settings.import.columns.size.times.map.with_index(1) {|i| "@#{i}" }.join(',')
+    variable = Settings.import.columns.map.with_index(1) do |column, i|
+      "#{column}=@#{i}"
+    end.join(',')
+
+    sql = <<"EOF"
+LOAD DATA LOCAL INFILE '#{tmp_file_name}'
+  INTO TABLE #{Rate.table_name}
+  FIELDS TERMINATED BY ',' (#{id}) SET #{variable}
+EOF
+    ActiveRecord::Base.connection.execute(sql)
+  end
+end
+
+rates = Rate.where('DATE(`time`) = ?', TARGET_DATE)
+unless rates.empty?
+  FileUtils.mkdir_p(BACKUP_DIR)
+
+  CSV.open(File.join(BACKUP_DIR, "#{TARGET_DATE}.csv"), 'w') do |csv|
+    rates.each do |rate|
+      csv << [rate.id, rate.time.strftime('%F %T'), rate.pair, rate.bid, rate.ask]
+    end
+  end
+end
+
+if rates_file == rates.size
+  FileUtils.rm(TARGET_FILES)
+end
