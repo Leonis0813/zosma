3df59cd736bde11f3eee61a8ac3be537ea0c36ee
Leonis0813 <Leonis.0813@gmail.com>
2018-08-21 13:46:58 +0100

    add logger

--- tools/compress.rb
+++ tools/compress.rb
@@ -8,6 +8,17 @@ require_relative '../config/initialize'
 TARGET_MONTH = (Date.today << 1).strftime('%Y-%m')
 EXPORT_DIR = File.join(APPLICATION_ROOT, Settings.import.backup_dir)
 
+logger = Logger.new(Settings.logger.path.compress)
+logger.formatter = proc do |severity, datetime, progname, message|
+  time = datetime.utc.strftime(Settings.logger.time_format)
+  log = "[#{severity}] [#{time}]: #{message}"
+  puts log if ENV['STDOUT'] == 'on'
+  "#{log}\n"
+end
+
+logger.info('Start compressing')
+start_time = Time.now
+
 Dir.mktmpdir(nil, File.join(APPLICATION_ROOT, Settings.import.tmp_dir)) do |dir|
   compressed_dir = File.join(dir, TARGET_MONTH)
   FileUtils.mkdir_p(compressed_dir)
@@ -18,8 +29,15 @@ Dir.mktmpdir(nil, File.join(APPLICATION_ROOT, Settings.import.tmp_dir)) do |dir|
 
     FileUtils.cp(Dir[File.join(EXPORT_DIR, "#{TARGET_MONTH}-*.csv")], compressed_dir)
     Dir.chdir(dir)
-    Dir["#{TARGET_MONTH}/*"].each {|file| Minitar::pack_file(file, out) }
+    Dir["#{TARGET_MONTH}/*"].each do |file|
+      Minitar::pack_file(file, out)
+      logger.info(:csv_file => File.basename(file), :lines => File.read(file).lines.size, :size => File.stat(file).size)
+    end
 
     out.close
   end
+
+  logger.info(:gzip_file => File.basename(gzip_file), :size => File.stat(gzip_file).size)
 end
+
+logger.info("Finish compressing (run_time: #{Time.now - start_time})")
--- tools/delete.rb
+++ tools/delete.rb
@@ -8,11 +8,32 @@ Dir[File.join(APPLICATION_ROOT, 'models/*')].each {|f| require_relative f }
 TARGET_DATE = (Date.today - 2).strftime('%F')
 TARGET_FILES = Dir[File.join(Settings.import.src_dir, "*_#{TARGET_DATE}.csv")]
 
+logger = Logger.new(Settings.logger.path.delete)
+logger.formatter = proc do |severity, datetime, progname, message|
+  time = datetime.utc.strftime(Settings.logger.time_format)
+  log = "[#{severity}] [#{time}]: #{message}"
+  puts log if ENV['STDOUT'] == 'on'
+  "#{log}\n"
+end
+
+logger.info('Start removing')
+start_time = Time.now
+
 rates_file = TARGET_FILES.inject([]) do |rates, file|
   CSV.read(file, :converters => :all).each do |rate|
     rates << [rate[0].strftime('%F %T'), rate[1], rate[2], rate[3]]
   end
+
+  logger.info(:file => File.basename(file), :lines => File.read(file).lines.size, :size => File.stat(file).size)
   rates
 end.uniq {|rate| [rate[0], rate[1]] }
 
-FileUtils.rm(TARGET_FILES) if rates_file.size == Rate.where('DATE(`time`) = ?', TARGET_DATE).size
+rates_db = Rate.where('DATE(`time`) = ?', TARGET_DATE)
+logger.info(:file_rate_size => rates_file.size, :db_rate_size => rates_db.size)
+
+if rates_file.size ==  rates_db.size
+  FileUtils.rm(TARGET_FILES)
+  logger.info(:removed_files => TARGET_FILES)
+end
+
+logger.info("Finish removing (run_time: #{Time.now - start_time})")
