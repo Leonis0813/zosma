d87d01b947a36a3d2ccd97fc5faa9ff43670eb3e
Leonis0813 <Leonis.0813@gmail.com>
2018-10-27 06:41:29 +0000

    remove candle sticks

--- tools/remove.rb
+++ tools/remove.rb
@@ -6,7 +6,6 @@ require_relative '../db/connect'
 Dir[File.join(APPLICATION_ROOT, 'models/*')].each {|f| require_relative f }
 
 TARGET_DATE = (Date.today - 2).strftime('%F')
-TARGET_FILES = Dir[File.join(Settings.import.src_dir, "*_#{TARGET_DATE}.csv")]
 
 logger = Logger.new(Settings.logger.path.remove)
 logger.formatter = proc do |severity, datetime, progname, message|
@@ -16,31 +15,37 @@ logger.formatter = proc do |severity, datetime, progname, message|
   "#{log}\n"
 end
 
+def remove(directory, target_class, index_key)
+  target_dir = Settings.import.file[directory]
+
+  backup_file = File.join(target_dir.backup_dir, "#{TARGET_DATE}.csv")
+  line_size = CSV.read(backup_file).size
+  logger.info(
+    :action => 'read',
+    :file => File.basename(backup_file),
+    :lines => line_size,
+    :size => File.stat(backup_file).size
+  )
+
+  db_size = target_class.where("DATE(`#{index_key}`) = ?", TARGET_DATE).size
+  logger.info(:action => 'compare', :backup_file_size => line_size, :db_size => db_size)
+
+  if line_size == db_size
+    src_files = Dir[File.join(target_dir.src_dir, "*_#{TARGET_DATE}.csv")]
+    FileUtils.rm(src_files)
+    logger.info(:action => 'remove', :removed_files => src_files)
+  end
+
+end
+
 logger.info("==== Start removing (date: #{TARGET_DATE})")
 start_time = Time.now
 
-rates_file = TARGET_FILES.inject([]) do |rates, file|
-  lines = CSV.read(file, :converters => :all)
-  lines.each {|rate| rates << [rate[0].strftime('%F %T'), rate[1], rate[2], rate[3]] }
-  logger.info(
-    :action => 'read',
-    :file => File.basename(file),
-    :lines => lines.size,
-    :size => File.stat(file).size
-  )
-  rates
-end.uniq {|rate| [rate[0], rate[1]] }
-
-rates_db = Rate.where('DATE(`time`) = ?', TARGET_DATE)
-logger.info(
-  :action => 'compare',
-  :file_rate_size => rates_file.size,
-  :db_rate_size => rates_db.size
-)
-
-if rates_file.size ==  rates_db.size
-  FileUtils.rm(TARGET_FILES)
-  logger.info(:action => 'remove', :removed_files => TARGET_FILES)
+[
+  ['rate', Rate, 'time'],
+  ['candle_stick', CandleStick, 'to'],
+].each do |directory, target_class, index_key|
+  remove(directory, target_class, index_key)
 end
 
 logger.info("==== Finish removing (run_time: #{Time.now - start_time})")
