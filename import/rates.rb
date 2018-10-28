0e09dbe3ad8e95b7b38983df7db38512b6efb1aa
Leonis0813 <Leonis.0813@gmail.com>
2018-10-28 04:04:51 +0000

    add args to rates

--- import/rates.rb
+++ import/rates.rb
@@ -6,8 +6,6 @@ require_relative '../config/initialize'
 require_relative '../db/connect'
 Dir['models/*'].each {|f| require_relative "../#{f}" }
 
-TARGET_DATE = (Date.today - 2).strftime('%F')
-TARGET_FILES = Dir[File.join(Settings.import.file.rate.src_dir, "*_#{TARGET_DATE}.csv")]
 BACKUP_DIR = File.join(APPLICATION_ROOT, Settings.import.file.rate.backup_dir)
 
 logger = Logger.new(Settings.logger.path.import)
@@ -18,67 +16,106 @@ logger.formatter = proc do |severity, datetime, progname, message|
   "#{log}\n"
 end
 
-logger.info("==== Start importing rates (date: #{TARGET_DATE})")
-start_time = Time.now
+begin
+  from = ARGV.find {|arg| arg.start_with?('--from=') }
+  from = from ? Date.parse(from.match(/\A--from=(.*)\z/)[1]) : (Date.today - 2)
+  to = ARGV.find {|arg| arg.start_with?('--to=') }
+  to = to ? Date.parse(to.match(/\A--to=(.*)\z/)[1]) : (Date.today - 2)
+rescue Exception => e
+  logger.error(e.backtrace.join("\n"))
+  raise e
+end
 
 Dir.mktmpdir(nil, File.join(APPLICATION_ROOT, Settings.import.tmp_dir)) do |dir|
+  (from.strftime('%Y-%m')..to.strftime('%Y-%m')).each do |yearmonth|
+    tar_gz_file = File.join(BACKUP_DIR, "#{yearmonth}.tar.gz")
+    csv_files = Dir[File.join(BACKUP_DIR, "#{yearmonth}-*.csv")]
+
+    if File.exists?(tar_gz_file)
+    # unpack backup_file to tmp dir
+    elsif not csv_files.empty?
+      FileUtils.cp(csv_files, dir)
+    else
+      csv_files = File.join(Settings.import.file.rate.src_dir, "*_#{yearmonth}-*.csv")
+      FileUtils.cp(Dir[csv_files], dir)
+    end
+  end
+
   tmp_file_name = File.join(dir, 'rates.csv')
 
-  TARGET_FILES.each do |file|
-    CSV.open(tmp_file_name, 'w') do |csv|
-      rates = CSV.read(file, :converters => :all).map do |rate|
-        [rate[0].strftime('%F %T'), rate[1], rate[2], rate[3]]
+  (from..to).each do |date|
+    date_string = date.strftime('%F')
+    csv_file = File.join(dir, "#{date_string}.csv")
+    target_files = if File.exists?(csv_file)
+                     [csv_file]
+                   else
+                     Dir[File.join(dir, "*_#{date_string}.csv")]
+                   end
+
+    target_files.each do |csv_file|
+      CSV.open(tmp_file_name, 'w') do |csv|
+        rates = CSV.read(csv_file, :converters => :all).map do |rate|
+          [rate[0].strftime('%F %T'), rate[1], rate[2], rate[3]]
+        end
+        logger.info(
+          :action => 'read',
+          :file => File.basename(file),
+          :size => File.stat(file).size
+        )
+
+        before_size = rates.size
+        rates.uniq! {|rate| [rate[0], rate[1]] }
+        logger.info(
+          :action => 'unique',
+          :before_size => before_size,
+          :after_size => rates.size
+        )
+
+        rates.each {|rate| csv << rate }
       end
-      logger.info(:action => 'read', :file => File.basename(file), :size => File.stat(file).size)
-      before_size = rates.size
-      rates.uniq! {|rate| [rate[0], rate[1]] }
 
-      logger.info(:action => 'unique', :before_size => before_size, :after_size => rates.size)
+      headers = Settings.import.file.rate.headers
+      ids = headers.size.times.map {|i| "@#{i + 1}" }
+      variables = headers.map.with_index(1) {|header, i| "#{header}=@#{i}" }
 
-      rates.each {|rate| csv << rate }
-    end
-
-    headers = Settings.import.file.rate.headers
-    ids = headers.size.times.map {|i| "@#{i + 1}" }
-    variables = headers.map.with_index(1) {|header, i| "#{header}=@#{i}" }
-
-    sql = <<"EOF"
+      sql = <<"EOF"
 LOAD DATA LOCAL INFILE '#{tmp_file_name}'
   INTO TABLE #{Rate.table_name}
   FIELDS TERMINATED BY ',' (#{ids.join(',')}) SET #{variables.join(',')}
 EOF
 
-    sql_start = Time.now
-    ActiveRecord::Base.connection.execute(sql)
-    logger.info(
-      :action => 'load',
-      :line => File.read(tmp_file_name).lines.size,
-      :runtime => Time.now - sql_start
-    )
+      rate_size = File.read(tmp_file_name).lines.size
+      sql_start = Time.now
+      ActiveRecord::Base.connection.execute(sql)
+      logger.info(
+        :action => 'load',
+        :line => rate_size,
+        :runtime => Time.now - sql_start
+      )
 
-    rate_size = CSV.read(tmp_file_name).size
-    sql = "ALTER TABLE #{Rate.table_name} AUTO_INCREMENT = #{rate_size + 1}"
-    ActiveRecord::Base.connection.execute(sql)
-  end
-end
-
-rates = Rate.where('DATE(`time`) = ?', TARGET_DATE)
-unless rates.empty?
-  FileUtils.mkdir_p(BACKUP_DIR)
-
-  backup_file = File.join(BACKUP_DIR, "#{TARGET_DATE}.csv")
-  CSV.open(backup_file, 'w') do |csv|
-    rates.each do |rate|
-      csv << [rate.id, rate.time.strftime('%F %T'), rate.pair, rate.bid, rate.ask]
+      sql = "ALTER TABLE #{Rate.table_name} AUTO_INCREMENT = #{rate_size + 1}"
+      ActiveRecord::Base.connection.execute(sql)
     end
+  end
 
-    logger.info(
-      :action => 'backup',
-      :file => File.basename(backup_file),
-      :lines => rates.size,
-      :size => File.stat(backup_file).size
-    )
+  backup_file = File.join(BACKUP_DIR, "#{date_string}.csv")
+  unless File.exists?(backup_file)
+    rates = Rate.where('DATE(`time`) = ?', date_string)
+    unless rates.empty?
+      FileUtils.mkdir_p(BACKUP_DIR)
+
+      CSV.open(backup_file, 'w') do |csv|
+        rates.each do |rate|
+          csv << [rate.id, rate.time.strftime('%F %T'), rate.pair, rate.bid, rate.ask]
+        end
+
+        logger.info(
+          :action => 'backup',
+          :file => File.basename(backup_file),
+          :lines => rates.size,
+          :size => File.stat(backup_file).size
+        )
+      end
+    end
   end
 end
-
-logger.info("==== Finish importing rates (run_time: #{Time.now - start_time})")
