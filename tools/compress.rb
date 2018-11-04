ac8b65d0ccaa8e9c26e4f48a6459f3471563de01
Leonis <Leonis.0813@gmail.com>
2018-11-04 22:44:52 +0900

    Fix bug compress (#17)
    * modify export_dir
    
    * update CHANGELOG.md

--- tools/compress.rb
+++ tools/compress.rb
@@ -6,7 +6,6 @@ require 'zlib'
 require_relative '../config/initialize'
 
 TARGET_MONTH = (Date.today << 1).strftime('%Y-%m')
-EXPORT_DIR = File.join(APPLICATION_ROOT, Settings.import.backup_dir)
 
 logger = Logger.new(Settings.logger.path.compress)
 logger.formatter = proc do |severity, datetime, progname, message|
@@ -20,33 +19,36 @@ logger.info("==== Start compressing (month: #{TARGET_MONTH})")
 start_time = Time.now
 
 Dir.mktmpdir(nil, File.join(APPLICATION_ROOT, Settings.import.tmp_dir)) do |dir|
-  compressed_dir = File.join(dir, TARGET_MONTH)
-  FileUtils.mkdir_p(compressed_dir)
+  %w[ rate candle_stick ].each do |type|
+    export_dir = File.join(APPLICATION_ROOT, Settings.import.file[type].backup_dir)
+    compressed_dir = File.join(dir, TARGET_MONTH)
+    FileUtils.mkdir_p(compressed_dir)
 
-  gzip_file = File.join(EXPORT_DIR, "#{TARGET_MONTH}.tar.gz")
-  Zlib::GzipWriter.open(gzip_file, Zlib::BEST_COMPRESSION) do |gzip|
-    out = Minitar::Output.new(gzip)
+    gzip_file = File.join(export_dir, "#{TARGET_MONTH}.tar.gz")
+    Zlib::GzipWriter.open(gzip_file, Zlib::BEST_COMPRESSION) do |gzip|
+      out = Minitar::Output.new(gzip)
 
-    FileUtils.cp(Dir[File.join(EXPORT_DIR, "#{TARGET_MONTH}-*.csv")], compressed_dir)
-    Dir.chdir(dir)
-    Dir["#{TARGET_MONTH}/*"].each do |file|
-      Minitar::pack_file(file, out)
-      logger.info(
-        :action => 'pack',
-        :csv_file => File.basename(file),
-        :lines => File.read(file).lines.size,
-        :size => File.stat(file).size,
-      )
+      FileUtils.cp(Dir[File.join(export_dir, "#{TARGET_MONTH}-*.csv")], compressed_dir)
+      Dir.chdir(dir)
+      Dir["#{TARGET_MONTH}/*"].each do |file|
+        Minitar::pack_file(file, out)
+        logger.info(
+          :action => 'pack',
+          :csv_file => File.basename(file),
+          :lines => File.read(file).lines.size,
+          :size => File.stat(file).size,
+        )
+      end
+
+      out.close
     end
 
-    out.close
+    logger.info(
+      :action => 'compress',
+      :gzip_file => File.basename(gzip_file),
+      :size => File.stat(gzip_file).size
+    )
   end
-
-  logger.info(
-    :action => 'compress',
-    :gzip_file => File.basename(gzip_file),
-    :size => File.stat(gzip_file).size
-  )
 end
 
 logger.info("==== Finish compressing (run_time: #{Time.now - start_time})")
