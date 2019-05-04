b88916e3df338887cefebf16615da077f72beb86
Leonis0813 <Leonis.0813@gmail.com>
2019-05-04 00:02:29 +0000

    fix Style/ClassCheck

--- lib/zosma_logger.rb
+++ lib/zosma_logger.rb
@@ -7,7 +7,7 @@ class ZosmaLogger < Logger
 
     self.formatter = proc do |severity, datetime, progname, message|
       time = datetime.utc.strftime(Settings.logger.time_format)
-      message = message.to_json if message.kind_of?(Hash)
+      message = message.to_json if message.is_a?(Hash)
       log = "[#{severity}] [#{time}]: #{message}"
       puts log if ENV['STDOUT'] == 'on'
       "#{log}\n"
