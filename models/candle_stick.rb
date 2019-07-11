f8a84957050d15f4b416a00ecf8e21635b64562c
Leonis0813 <Leonis.0813@gmail.com>
2019-07-11 12:28:55 +0000

    add validation to models

--- models/candle_stick.rb
+++ models/candle_stick.rb
@@ -1,2 +1,13 @@
 class CandleStick < ActiveRecord::Base
+  PAIR_LIST = %w[AUDJPY CADJPY CHFJPY EURJPY EURUSD GBPJPY NZDJPY USDJPY]
+  TIME_FRAME_LIST = %w[M1 M5 M15 M30 H1 H4 D1 W1 MN1]
+
+  validates :from. :to, :pair, :time_frame, :open, :close, :high, :low,
+            presence: {message: 'absent'}
+  validates :pair,
+            inclusion: {in: PAIR_LIST, message: 'invalid'}
+  validates :time_frame,
+            inclusion: {in: TIME_FRAME_LIST, message: 'invalid'}
+  validates :open, :close, :high, :low,
+            numericality: {greater_than: 0, message: 'invalid'}
 end
--- models/moving_average.rb
+++ models/moving_average.rb
@@ -1,2 +1,16 @@
 class MovingAverage < ActiveRecord::Base
+  PAIR_LIST = %w[AUDJPY CADJPY CHFJPY EURJPY EURUSD GBPJPY NZDJPY USDJPY]
+  TIME_FRAME_LIST = %w[M1 M5 M15 M30 H1 H4 D1 W1 MN1]
+  PERIOD_LIST = [25, 50, 75, 100, 150, 200]
+
+  validates :time, :pair, :time_frame, :period, :value,
+            presence: {message: 'absent'}
+  validates :pair,
+            inclusion: {in: PAIR_LIST, message: 'invalid'}
+  validates :time_frame,
+            inclusion: {in: TIME_FRAME_LIST, message: 'invalid'}
+  validates :period,
+            inclusion: {in:  PERIOD_LIST, message: 'invalid'}
+  validates :value,
+            numericality: {greater_than: 0, message: 'invalid'}
 end
