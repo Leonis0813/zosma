b7797b2145225d62deb46f04118778fe2775ba68
Leonis0813 <Leonis.0813@gmail.com>
2019-07-11 11:46:42 +0000

    modify activerecord logger

--- db/connect.rb
+++ db/connect.rb
@@ -1,5 +1,9 @@
 require 'active_record'
 require 'mysql2'
 require_relative '../config/initialize'
+require_relative '../lib/denebola_logger'
 
-ActiveRecord::Base.establish_connection(Settings.mysql.to_h)
+ActiveRecord::Base.logger = DenebolaLogger.new(Settings.logger.path.database)
+ActiveRecord::Base.logger.level = Settings.logger.level.to_sym
+settings = Settings.mysql.map {|key, value| [key.to_s, value] }.to_h
+ActiveRecord::Base.establish_connection(settings)
