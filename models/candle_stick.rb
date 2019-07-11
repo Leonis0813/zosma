7724230d9f106f57cc96c76046ef46467e52f6d5
Leonis0813 <Leonis.0813@gmail.com>
2019-07-11 12:41:50 +0000

    fix style

--- models/candle_stick.rb
+++ models/candle_stick.rb
@@ -1,8 +1,8 @@
 class CandleStick < ActiveRecord::Base
-  PAIR_LIST = %w[AUDJPY CADJPY CHFJPY EURJPY EURUSD GBPJPY NZDJPY USDJPY]
-  TIME_FRAME_LIST = %w[M1 M5 M15 M30 H1 H4 D1 W1 MN1]
+  PAIR_LIST = %w[AUDJPY CADJPY CHFJPY EURJPY EURUSD GBPJPY NZDJPY USDJPY].freeze
+  TIME_FRAME_LIST = %w[M1 M5 M15 M30 H1 H4 D1 W1 MN1].freeze
 
-  validates :from. :to, :pair, :time_frame, :open, :close, :high, :low,
+  validates :from, :to, :pair, :time_frame, :open, :close, :high, :low,
             presence: {message: 'absent'}
   validates :pair,
             inclusion: {in: PAIR_LIST, message: 'invalid'}
