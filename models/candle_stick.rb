e0e988eaab9224b8474df651a24a09b75f5d22dd
Leonis0813 <Leonis.0813@gmail.com>
2021-01-06 15:17:22 +0000

    refactor

--- models/candle_stick.rb
+++ models/candle_stick.rb
@@ -10,15 +10,4 @@ class CandleStick < ApplicationRecord
             inclusion: {in: TIME_FRAME_LIST, message: 'invalid'}
   validates :open, :close, :high, :low,
             numericality: {greater_than: 0, message: 'invalid'}
-
-  def self.load_data(file_name)
-    headers = self.attribute_names - %w[id created_at updated_at]
-    ids = headers.size.times.map {|i| "@#{i + 1}" }
-    variables = headers.map.with_index(1) {|header, i| "`#{header}`=@#{i}" }
-    variables += %w[created_at=now() updated_at=now()]
-    super(file_name, ids, variables, self.table_name)
-
-    sql = "ALTER TABLE #{self.table_name} AUTO_INCREMENT = #{last.id + 1}"
-    ActiveRecord::Base.connection.execute(sql)
-  end
 end
