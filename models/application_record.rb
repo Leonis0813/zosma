537b29efa250600192ffa46e7e4c6518ddf1d38b
Leonis0813 <Leonis.0813@gmail.com>
2021-01-06 15:22:29 +0000

    fix style

--- models/application_record.rb
+++ models/application_record.rb
@@ -4,15 +4,15 @@ class ApplicationRecord < ActiveRecord::Base
   cattr_accessor :logger
 
   def self.load_data(file_name)
-    headers = self.attribute_names - %w[id created_at updated_at]
-    ids = headers.size.times.map {|i| "@#{i + 1}" }
+    headers = attribute_names - %w[id created_at updated_at]
+    ids = Array.new(headers.size) {|i| "@#{i + 1}" }
     variables = headers.map.with_index(1) {|header, i| "`#{header}`=@#{i}" }
     variables += %w[created_at=now() updated_at=now()]
 
     sql = <<-"SQL"
-LOAD DATA LOCAL INFILE '#{file_name}'
-IGNORE INTO TABLE #{self.table_name}
-FIELDS TERMINATED BY ',' (#{ids.join(',')}) SET #{variables.join(',')}
+      LOAD DATA LOCAL INFILE '#{file_name}'
+      IGNORE INTO TABLE #{table_name}
+      FIELDS TERMINATED BY ',' (#{ids.join(',')}) SET #{variables.join(',')}
     SQL
 
     file_line_size = File.read(file_name).lines.size
@@ -20,6 +20,6 @@ FIELDS TERMINATED BY ',' (#{ids.join(',')}) SET #{variables.join(',')}
     connection.execute(sql)
     logger.info(action: 'load', line: file_line_size, runtime: Time.now - sql_start)
 
-    connection.execute("ALTER TABLE #{self.table_name} AUTO_INCREMENT = #{last.id + 1}")
+    connection.execute("ALTER TABLE #{table_name} AUTO_INCREMENT = #{last.id + 1}")
   end
 end
