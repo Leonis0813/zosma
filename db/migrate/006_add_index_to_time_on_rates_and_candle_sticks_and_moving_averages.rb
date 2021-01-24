31dd56d1e3c970269fc1491c2a886871b52d4844
Leonis0813 <Leonis.0813@gmail.com>
2021-01-24 16:09:50 +0000

    add indexes

---
+++ db/migrate/006_add_index_to_time_on_rates_and_candle_sticks_and_moving_averages.rb
@@ -0,0 +1,7 @@
+class AddIndexToTimeOnRatesAndCandleSticksAndMovingAverages < ActiveRecord::Migration[4.2]
+  def change
+    add_index :rates, :time
+    add_index :candle_sticks, :to
+    add_index :moving_averages, :time
+  end
+end
