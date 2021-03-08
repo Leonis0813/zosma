cb2dc8899d3548d1640d38f93d5b81db7efa06cc
Leonis0813 <Leonis.0813@gmail.com>
2021-03-08 14:57:59 +0000

    add log to tools/remove.rb

--- tools/remove.rb
+++ tools/remove.rb
@@ -5,35 +5,34 @@ Dir[File.join(APPLICATION_ROOT, 'models/*')].each {|f| require_relative f }
 TARGET_DATE = (Date.today - 2).strftime('%F')
 logger = ZosmaLogger.new(Settings.logger.path.remove)
 
-logger.info("==== Start removing (date: #{TARGET_DATE})")
-start_time = Time.now
+logger.info('======== Start Remove ========')
+logger.info("Date: #{TARGET_DATE}")
 
 [
   ['rate', Rate, 'time'],
   ['candle_stick', CandleStick, 'to'],
   ['moving_average', MovingAverage, 'time'],
 ].each do |directory, target_class, index_key|
+  logger.info("==== Remove #{directory}")
+
   target_dir = Settings.import.file[directory]
 
   backup_file = File.join(target_dir.backup_dir, "#{TARGET_DATE}.csv")
   next unless File.exist?(backup_file)
 
-  line_size = CSV.read(backup_file).size
-  logger.info(
-    action: 'read',
-    file: File.basename(backup_file),
-    lines: line_size,
-    size: File.stat(backup_file).size,
-  )
+  lines = CSV.read(backup_file).size
+  bytes = File.stat(backup_file).size
+  logger.info("Read #{backup_file} (#{lines} lines, #{bytes} bytes)")
 
-  db_size = target_class.where("DATE(`#{index_key}`) = ?", TARGET_DATE).size
-  logger.info(action: 'compare', backup_file_size: line_size, db_size: db_size)
+  record_size = target_class.where("DATE(`#{index_key}`) = ?", TARGET_DATE).size
+  logger.info("Read #{target_class.table_name} table (#{record_size} records)")
 
   next unless line_size == db_size
 
   src_files = Dir[File.join(target_dir.src_dir, "*_#{TARGET_DATE}.csv")]
   FileUtils.rm(src_files)
-  logger.info(action: 'remove', removed_files: src_files)
+  logger.info("Remove files: #{src_files}")
 end
+logger.info('====')
 
-logger.info("==== Finish removing (run_time: #{Time.now - start_time})")
+logger.info('======== Finish Remove ========')
