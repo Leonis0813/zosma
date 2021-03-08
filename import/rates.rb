085679bcf50c578da6264325e4045813830a0af2
Leonis0813 <Leonis.0813@gmail.com>
2021-03-08 13:49:25 +0000

    add log to import/rates.rb

--- import/rates.rb
+++ import/rates.rb
@@ -5,6 +5,8 @@ require_relative '../models/rate'
 logger = ZosmaLogger.new(Settings.logger.path.import)
 ApplicationRecord.zosma_logger = logger
 
+logger.info('======== Start Import ========')
+
 begin
   from = ARGV.find {|arg| arg.start_with?('--from=') }
   from = from ? Date.parse(from.match(/\A--from=(.*)\z/)[1]) : (Date.today - 7)
@@ -15,29 +17,41 @@ rescue ArgumentError => e
   raise e
 end
 
+logger.info('Parameter:')
+logger.info("  from: #{from}")
+logger.info("  to: #{to}")
+
 (from..to).each do |date|
   Dir.mktmpdir(nil, File.join(APPLICATION_ROOT, Settings.import.tmp_dir)) do |tmp_dir|
     tmp_file_name = File.join(tmp_dir, 'rates.csv')
 
     file_pattern = "*_#{date.strftime('%F')}.csv"
+    logger.info("==== Import #{file_pattern}")
+
     target_files = Dir[File.join(Settings.import.file.rate.src_dir, file_pattern)]
+    logger.info("Target Files: #{target_files}")
+
     FileUtils.cp(target_files, tmp_dir)
 
     Dir[File.join(tmp_dir, file_pattern)].each do |file|
+      logger.info("== Import #{file}")
+
       CSV.open(tmp_file_name, 'w') do |csv|
         rates = CSV.read(file, converters: :all).map do |rate|
           [rate[0].strftime('%F %T'), rate[1], rate[2], rate[3]]
         end
-        logger.info(action: 'read', file: file, size: File.stat(file).size)
+        logger.info("Read #{rates.size} lines (#{File.stat(file).size} bytes)")
 
-        before_size = rates.size
         rates.uniq! {|rate| [rate[0], rate[1]] }
-        logger.info(action: 'unique', before_size: before_size, after_size: rates.size)
 
         rates.each {|rate| csv << rate }
+        logger.info("Write #{rates.size} lines to #{tmp_file_name}")
       end
 
       Rate.load_data(tmp_file_name)
+      logger.info('Load Data to rates table')
     end
   end
 end
+
+logger.info('======== Finish Import ========')
